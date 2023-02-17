ee5c342561638e6841945098d424969f6341889d
Samuel Fajardo <samuelfajardo28@gmail.com>
2023-02-16 20:30:30 -0500

    Se realiza implementaciòn de barra de menu mobile, junto con su configuraciòn para que se vea la pagina diferente en celulares y en computadores

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu")
+const menu_barra_lateral_mobile = document.querySelector(".menu")
+const menumobile = document.querySelector(".mobile-menu")
+
 menuEmail.addEventListener("click", toggleDesktopmenu)
+menu_barra_lateral_mobile.addEventListener("click", togglemobilemenu)
+
 // se coloca el toggle por lo que se quiere mostrar con el cursor y desaparecer cuando no este 
+
 function toggleDesktopmenu(){
     desktopmenu.classList.toggle("inactive");
+}
+
+function togglemobilemenu(){
+    menumobile.classList.toggle("inactive")
 }
